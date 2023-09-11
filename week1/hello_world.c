da22eb1464930e89ab3a17d748140f8066b1bc7f
Nicole <z5206727@unsw.edu.au>
2023-09-11 23:31:12 +1000

    tutorial code

---
+++ week1/hello_world.c
@@ -0,0 +1,43 @@
+// Basic Hello World Program
+// Nicole, 23T3 Week 1
+// A program to print "Hello World"
+
+#include <stdio.h>
+
+int main(void) {
+    // printing a cat
+    printf("Hello World\n");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Change the code to print this cat
+/*  
+ /\___/\
+(=' w '=)
+ */
