7838e38b343bf1cb873b0109a0f7e459926335bf
Nicole <z5206727@unsw.edu.au>
2023-09-26 15:39:38 +1000

    tutorial code ready

---
+++ week3/vet.c
@@ -0,0 +1,22 @@
+// Nicole, 23T2 Week 3
+// Tutorial structs, enums demo
+
+#include <stdio.h>
+
+// what are the different parts of this enum?
+// create an enum to store different types of pets
+
+
+// create a struct to store information for a pet
+// include an int, double, enum, char
+
+int main(void) {
+
+    // create a profile for a pet
+
+    // initialise the fields inside the struct
+    
+    // print out the stored information
+
+    return 0;
+}
