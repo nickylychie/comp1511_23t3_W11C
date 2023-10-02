773d44c921cb4f7d3f0bfb9dbd11ccc1457d87e0
Nicole <z5206727@unsw.edu.au>
2023-10-02 14:26:31 +1100

    Tutorial code ready

---
+++ week4/scan_100_temperatures.c
@@ -0,0 +1,9 @@
+// Nicole, Week 4 23T3
+// Tutorial demo code
+
+#include <stdio.h>
+
+int main(void) {
+
+    return 0;
+}
---
+++ week4/scan_temperatures.c
@@ -0,0 +1,13 @@
+// Nicole, Week 4 23T3
+// Tutorial demo code
+
+#include <stdio.h>
+
+#define SIZE 6
+
+int main(void) {
+
+    // scan 6 temperatures into an array
+
+    return 0;
+}
