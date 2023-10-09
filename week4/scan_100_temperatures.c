062596f2ee94052f50c57bebf0a33c7628b320b5
Nicole <z5206727@unsw.edu.au>
2023-10-09 22:00:12 +1100

    Tutorial demo code

--- week4/scan_100_temperatures.c
+++ week4/scan_100_temperatures.c
@@ -3,7 +3,44 @@
 
 #include <stdio.h>
 
+#define SIZE 100
+
 int main(void) {
+
+    // ask for temperatues
+    printf("Give me some temps pls\n");
+
+    // initialise variable
+    int temp[SIZE];
+    int highest_temp;
+
+    // scan in 6 temperatures
+    int length = 0;
+    int scanf_return = scanf("%d", temp[0]);
+    while (scanf_return == 1) {
+        length++;
+        scanf_return = scanf("%d", temp[length]);
+    }
+
+    if (length != 0) {
+         // find highest
+    // assume highest is the first element in array
+    highest_temp = temp[0];
+
+    // compare to every element
+    // if there is a higher temp, then switch it
+    int i = 0;
+    while (i < length) {
+        if (temp[i] > highest_temp) {
+            highest_temp = temp[i];
+        }
+        i++;
+    }
+
+    // print highest
+    printf("Highest temperature is %d\n", highest_temp);
+    }
+   
 
     return 0;
 }
--- week4/scan_temperatures.c
+++ week4/scan_temperatures.c
@@ -7,7 +7,36 @@
 
 int main(void) {
 
-    // scan 6 temperatures into an array
+    // ask for temperatues
+    printf("Give me 6 temps pls\n");
+
+    // initialise variable
+    int temp[SIZE];
+    int highest_temp;
+
+    // scan in 6 temperatures
+    int i = 0;
+    while (i < SIZE) {
+        scanf("%d", &temp[i]);
+        i++;
+    }
+
+    // find highest
+    // assume highest is the first element in array
+    highest_temp = temp[0];
+
+    // compare to every element
+    // if there is a higher temp, then switch it
+    i = 0;
+    while (i < SIZE) {
+        if (temp[i] > highest_temp) {
+            highest_temp = temp[i];
+        }
+        i++;
+    }
+
+    // print highest
+    printf("Highest temperature is %d\n", highest_temp);
 
     return 0;
 }
